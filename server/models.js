eba86144601114c716c05020d7a9ab6434f0ba62
Michelsen93 <om.michelsen@gmail.com>
2017-03-27 13:25:44 +0200

    fixed path of dependencies

---
+++ server/models.js
@@ -0,0 +1,31 @@
+var ottoman = require("ottoman");
+
+ottoman.bucket = require("./app").bucket;
+
+var PersonModel = ottoman.model("Person", {
+   timestamp: {
+       type: "Date",
+       default: function () { return new Date(); }
+   },
+    name: {
+       first: "string",
+        last: "string"
+    },
+    email: "string",
+    comments:[
+        {
+            ref: "Comment"
+        }
+    ]
+});
+
+var CommentModel = ottoman.model("Comment", {
+    timestamp:{
+        type: "Date",
+        default: function(){return new Date();}
+    },
+    message: "string"
+});
+
+module.exports.PersonModel = PersonModel;
+module.exports.CommentModel = CommentModel;
