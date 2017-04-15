58a77cfee546b0e667d43d4255d25554e8da0b94
Michelsen93 <om.michelsen@gmail.com>
2017-04-15 20:20:51 +0200

    Added empty functions to be implemented

---
+++ ajax/Ajax.js
@@ -0,0 +1,90 @@
+/**
+ * Created by ole-martin on 15.04.2017.
+ */
+
+/**
+ * Gets a competition
+ *
+ **/
+function getCompetitionById(){
+    
+}
+
+/**
+ * Gets all competitions
+ *
+ **/
+function getCompetitions(){
+    
+}
+
+/**
+ * Saves a competition
+ *
+ **/
+function postCompetition() {
+    
+}
+
+/**
+ * Creates a new user
+ *
+ **/
+function postUser() {
+    
+}
+
+
+/**
+ * Adds a team to a competition
+ *
+ **/
+function postTeam(){
+    
+}
+
+/**
+ * Removes a team
+ *
+ **/
+function deleteTeam() {
+    
+}
+
+
+/**
+ * Gets all persons
+ *
+ **/
+function getAllPersons() {
+    
+}
+
+/**
+ * Gets a person
+ *
+ **/
+function getPerson() {
+    
+}
+
+/**
+ * Gets all weapongroups
+ *
+ **/
+function getWeaponGroups(){
+    
+}
+
+/**
+ * Gets all weapon classes
+ *
+ **/
+function getWeaponClasses() {
+    
+}
+
+/**
+ *
+ *
+ **/
