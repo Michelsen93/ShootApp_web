65fc303c67af8155ed967f3ec8e1c4772d0ed931
Fossen93 <sondrefr@gmail.com>
2017-04-16 18:57:06 +0200

    nytt stevne commit

--- NyttStevneJs.js
+++ NyttStevneJs.js
@@ -2,6 +2,20 @@
  * Created by sondrefr on 22.03.2017.
  */
 
+function stevne (stevneNummer, dato){
+    this.stevneNummer = stevneNummer;
+    this.dato = dato;
+}
+
+function lagreStevne(){
+    n = document.getElementById("stevneNummer").value;
+    o = document.getElementById("dato").value;
+
+
+    k = JSON.parse(stevne1)
+    alert(stevne1)
+}
+
 var antallStandplass = 0;
 function addStandplass() {
     antallStandplass++;
@@ -42,7 +56,8 @@ function addVapengruppe(div){
     i--;
 
     var div = document.createElement('div');
-    div.innerHTML = '<input type="checkbox" id="'+p+'" value="'+p+'"> '+p+'<br>';
+    div.innerHTML = '<input type="checkbox" id="'+p+'" value="'+p+'"> '+p+'' +
+        '<button type="button" class="close" aria-label="Close" onClick="removeVapengruppe(this)"><span aria-hidden="true">&times;</span></button><br>';
 
     if (antVapengrupper%2==0){
         document.getElementById('vapengruppe1').appendChild(div)
@@ -52,6 +67,13 @@ function addVapengruppe(div){
 
 
     antVapengrupper++;
+}
+
+function removeVapengruppe(div) {
+    document.getElementById('vapengruppe2').removeChild(div.parentNode);
+    document.getElementById('vapengruppe1').removeChild(div.parentNode);
+
+    antVapengrupper--;
 }
 
 var checkedVapen = "";
@@ -66,4 +88,52 @@ function fillVapenBox(){
     }
 
     document.getElementById("vapentxt").value = checkedVapen;
+}
+
+var j = 0;
+function nyKlasse(){
+    if(j<1) {
+        var div = document.createElement('div');
+        div.innerHTML = '<br><input type="text" id="nyKlasse">  ' +
+            '<input type="button" value="Legg til" onClick="addKlasse(this)"><br> ';
+
+        document.getElementById('footerKlasse').appendChild(div)
+        i++;
+    }
+
+}
+
+var antKlasser = 0;
+function addKlasse(div){
+    var p = document.getElementById('nyKlasse').value;
+
+    document.getElementById('footerKlasse').removeChild(div.parentNode);
+    i--;
+
+    var div = document.createElement('div');
+    div.innerHTML = '<input type="checkbox" id="'+p+'" value="'+p+'"> '+p+'' +
+        '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button><br>';
+
+    if (antKlasser%2==0){
+        document.getElementById('klasse1').appendChild(div)
+    }else{
+        document.getElementById('klasse2').appendChild(div)
+    }
+
+
+    antKlasser++;
+}
+
+var checkedKlasse = "";
+function fillKlasseBox(){
+
+    for (i = 0; i < document.getElementById('klasse1_form').length; i++){
+
+        if(document.getElementById('klasse1_form')[i].checked){
+            checkedKlasse = checkedKlasse.concat(document.getElementById('klasse1_form')[i].value + '\n');
+
+        }
+    }
+
+    document.getElementById("klassetxt").value = checkedKlasse;
 }
